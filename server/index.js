0871b20dc6268f3ca8b40de45ca8df7bba31a291
sam <sam.a.mez@gmail.com>
2017-07-04 12:43:30 -0700

    Changed scripts to node_modules

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ app.use(express.static(path.resolve(__dirname + '/../dist/')));
 
 console.log("dirname" + path.resolve(__dirname + '/../node_modules/'));
 
-app.use('/scripts', express.static(path.resolve(__dirname + '/../node_modules/')));
+app.use('/node_modules', express.static(path.resolve(__dirname + '/../node_modules/')));
 
 app.listen(3000);
