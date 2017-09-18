1c045183a991590a20e52b8b32aaa482729d5e30
sam <sam.a.mez@gmail.com>
2017-09-17 21:17:56 -0700

    Added about description, changed quote divider content

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,12 @@
 var attach = function attach(element, listener, event, tf){
     if (element.attachEvent) {
-        element.attachEvent("on"+listener,event);
+        element.attachEvent("on"+listener, event);
     } else{
-        element.addEventListener(listener,event,tf);
+        element.addEventListener(listener, event, tf);
     }
 }
 
-var fadeOut = function fadeOut(element,startLevel,endLevel,duration,callback){
-    var fOInt,
-    opacity = startLevel;
-    fOInt = setInterval(function() {
-        if (opacity <= endLevel) {
-            element.style.opacity = endLevel;
-            element.style.filter = "alpha(opacity = " + endLevel + ")";
-            clearInterval(fOInt);
-            if( typeof callback == 'function') callback(true);
-        } else {
-            opacity -= 0.1;
-            element.style.opacity = opacity;
-            element.style.filter = "alpha(opacity = " + opacity * 100 + ")";
-        }
-    }, duration);
-}
-
 attach(window, 'load', function() {
-    // fadeOut(document.getElementById('loader-wrapper'), 1, 0, 50, function (cb) {
-    // });
     document.getElementById('loader-wrapper').className = 'slide-out';
     document.getElementById('loader-wrapper').visible = false;
 });
